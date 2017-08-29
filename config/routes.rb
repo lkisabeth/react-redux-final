8627e3e0966ab613163f4bc069c5ee656cb329d5
lukeymac <lukey.mac@outlook.com>
2017-08-29 19:38:53 -0400

    change stickynotes routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'sticky_notes/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :stickynotes
 end
